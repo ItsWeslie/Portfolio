1d1e5141c3178c7235fa820d6c16b14b70cbdae7
Sam Weslie Prabhakaran P <itssamwesliehere@gmail.com>
2026-09-21 18:27:43 +0530

    Mount interactive 3D avatar in About section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./education-record.css";
 import "./about-avatar.css";
 import { Toaster } from "sonner";
 import SoftwarePortfolio from "./pages/SoftwarePortfolio";
+import AboutAvatar from "./components/AboutAvatar";
 
 function App() {
   return (
     <>
       <Toaster position="bottom-right" richColors />
       <SoftwarePortfolio />
+      <AboutAvatar />
     </>
   );
 }
